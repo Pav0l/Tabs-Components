6906878579c0cecd6c5ee6eaeacb3836bdc0a638
Pav0l <pavol.karas@gmail.com>
2019-01-31 16:13:56 +0100

    Stretch goal - create carousel

---
+++ components/Tabs/CarouselTabs.js
@@ -0,0 +1,63 @@
+let contentItems = document.querySelectorAll('.tabs-item');
+
+// Modify current tabs links into left and right button for carousel
+let tabsLinks = document.querySelector('.tabs-links');
+tabsLinks.classList.add('carousel-tabs-link');
+let leftBtn = tabsLinks.children[0];
+let rightBtn = tabsLinks.children[3];
+leftBtn.innerText = "Left";
+rightBtn.innerText = "Right";
+tabsLinks.children[1].style = "display: none";
+tabsLinks.children[2].style = "display: none";
+
+leftBtn.addEventListener('click', slideCarouselLeft);
+rightBtn.addEventListener('click', slideCarouselRight);
+// end of tabs links modification
+
+// Create a position counter to stop it going to infinity when clicking btns on carousel
+let currentPosition;
+
+function slideCarouselRight() {
+    // Check which position (NodeList item) has 'tabs-item-selected' ON and remove it.
+   for (let i = 0; i < contentItems.length; i++) {
+       if (contentItems[i].classList.contains('tabs-item-selected')) {
+        currentPosition = Number(contentItems[i].dataset.tab)+1;
+        contentItems[i].classList.remove('tabs-item-selected')
+       }           
+   }
+
+   // Make sure the currentPosition counter does not exceed lenght of NodeList
+   if (currentPosition > contentItems.length) {
+       currentPosition = 1;
+    }
+
+    // Add the 'tabs-item-selected' to next item based on dataset count.
+   let nextItem = document.querySelector(`.tabs-item[data-tab="${currentPosition}"]`);
+   nextItem.classList.add('tabs-item-selected');
+
+   return currentPosition;
+}
+
+
+
+
+function slideCarouselLeft() {
+    // Check which position (NodeList item) has 'tabs-item-selected' ON and remove it.
+   for (let i = 0; i < contentItems.length; i++) {
+       if (contentItems[i].classList.contains('tabs-item-selected')) {
+        currentPosition = Number(contentItems[i].dataset.tab)-1;
+        contentItems[i].classList.remove('tabs-item-selected')
+       }           
+    }
+
+    // Make sure the currentPosition counter does not go below 0
+    if (currentPosition <= 0) {
+        currentPosition = contentItems.length;
+    }
+
+    // Add the 'tabs-item-selected' to next item based on dataset count.
+   let nextItem = document.querySelector(`.tabs-item[data-tab="${currentPosition}"]`);
+   nextItem.classList.add('tabs-item-selected');
+
+   return currentPosition;
+}
--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -20,7 +20,6 @@ class TabLink {
   select() {
     // Get all of the elements with the tabs-link class
     const links = document.querySelectorAll('.tabs-link');
-    console.log(links)
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
     // Array.from(links).forEach();
     for (let i = 0; i < links.length; i++) {
@@ -44,7 +43,6 @@ class TabItem {
   select() {
     // Select all ".tabs-item" elements from the DOM
     const items = document.querySelectorAll('.tabs-item');
-    console.log(items)
     // Remove the class "tabs-item-selected" from each element
     items.forEach(item => item.classList.remove('tabs-item-selected'));
     // Add a class named "tabs-item-selected" to this element
